659c2aeaa48fb46318e5a5729a46c7a748b14fe2
anna.gajda <anna.gajda96@gmail.com>
2018-10-07 14:46:06 +0200

    cokolwiek

---
+++ Database1/Database1/dbo/Tables/tabelka_cyferki.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[tabelka_cyferki] (
+    [jeden] NCHAR (10) NOT NULL,
+    [dwaa]  NCHAR (10) NULL,
+    CONSTRAINT [PK_tabelka_cyferki] PRIMARY KEY CLUSTERED ([jeden] ASC)
+);
+
